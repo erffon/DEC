7951ef6a59159c0f7a720b4efb2575c5151eaf2e
erfan home <erffon@gmail.com>
2022-05-31 00:41:43 +0430

    Revert "chenge header title to english"
    This reverts commit 7037b6547079bd34eb97589abe0c7e00b49ba537.

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- bootstrap css -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.rtl.min.css">
     <link rel="stylesheet" href="assets/css/custome-style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
 
@@ -14,6 +14,6 @@
     <script src="assets/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
 
-    <title>Dynamic Equity Calculator (DEC)</title>
+    <title>محاسبه‌گر سهام داینامیک</title>
 </head>
 
