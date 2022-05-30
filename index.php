80b0d267fe1c1f69a1d698ef41e2c70a81fe0f2e
erfan home <erffon@gmail.com>
2022-05-31 00:38:57 +0430

    changes back here

--- index.php
+++ index.php
@@ -9,19 +9,20 @@
 <div class="container">
     <div class="row align-items-center" style="padding-top:7%;">
 
-        <div class="col-12 col-md-6">
-            <div class="row mx-auto gap-5">
-                <h1 class="display-4"><strong>تقسیم سهام</strong> دیگر دغدغه نخواهد بود!</h1>
-                <p>با استفاده از محاسبه‌گر سهام داینامیک، به راحتی میزان سهام هر کدام از بنیان‌گذاران کسب‌وکارتان را بصورت ماهانه مشخص کنید و بدون دغدغه به گسترش بیزنس خود فکر کنید.</p>
-                <div class="row">
-                    <div class="col-6 col-md-4 d-flex justify-content-start"><button class="btn btn-lg btn-dark">ثبت نام رایگان <i class="bi bi-arrow-left"></i> </button></div>
-                    <div class="col-6 col-md-4 d-flex justify-content-start"><button class="btn btn-lg btn-outline-dark">اطلاعات بیشتر <i class="bi bi-arrow-left"></i> </button></div>
-                </div>
-            </div>
-        </div>
         <div class="col-12 col-md-6 d-flex justify-content-center ">
             <img src="assets\images\christopher-gower-m_HRfLhgABo-unsplash.jpg" alt="hero image" class="img-fluid">
         </div>
+        <div class="col-12 col-md-6">
+            <div class="row gap-5">
+                <h1 class="display-4"><strong>تقسیم سهام</strong> دیگر دغدغه نخواهد بود!</h1>
+                <p>با استفاده از محاسبه‌گر سهام داینامیک، به راحتی میزان سهام هر کدام از بنیان‌گذاران کسب‌وکارتان را بصورت ماهانه مشخص کنید و بدون دغدغه به گسترش بیزنس خود فکر کنید.</p>
+                <div class="row">
+                    <div class="col-6 col-md-4 d-flex justify-content-end"><button class="btn btn btn-dark">ثبت نام رایگان <i class="bi bi-arrow-left"></i> </button></div>
+                    <div class="col-6 col-md-4 d-flex justify-content-end"><button class="btn btn btn-outline-dark">اطلاعات بیشتر <i class="bi bi-arrow-left"></i> </button></div>
+                </div>
+            </div>
+        </div>
+        
 
     </div>
 </div>
--- navigator.php
+++ navigator.php
@@ -2,10 +2,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light px-3" style="border-radius:0px 0px 20px 20px;">
   <div class="container">
 
-    <!-- logo -->
-    <img class="navbar-brand me-3" src="assets\images\like.png" alt="لوگو محاسبه‌گر تقسیم داینامیک">
-    <a class="navbar-brand" href="#">محاسبه‌گر سهام داینامیک</a>
-
     <!-- toggle button -->
     <button class=" navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -14,23 +10,25 @@
    <!-- nav -->
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-3">
-        <li class="nav-item">
-          <a class="nav-link" href="#">تعرفه‌ها</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">درباره ما</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">بلاگ</a>
-        </li>
-
-          <div class="btn-group" role="group" aria-label="Basic outlined example">
-            <button type="button" class="btn btn-outline-dark">ورود</button>
-            <button type="button" class="btn btn-outline-dark">ثبت نام</button>
+      <div class="btn-group" role="group" aria-label="Basic outlined example">
+            <button type="button" class="btn btn-outline-dark">login</button>
+            <button type="button" class="btn btn-outline-dark">sign up</button>
           </div>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prices</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
         </li>
       </ul>
     </div>
+    <!-- logo -->
+    <a class="navbar-brand" href="#">Dynamic Equity Calculator</a>
+    <img class="navbar-brand me-3" src="assets\images\like.png" alt="لوگو محاسبه‌گر تقسیم داینامیک">
+    
   </div>
 </nav>
 </div>
