440f51333fe90f78ef5baafb9d3c660ef948bd32
erfan home <erffon@gmail.com>
2022-06-02 00:37:16 +0430

    added tiles

--- index.php
+++ index.php
@@ -11,11 +11,11 @@
 
 
         <div class="col-12 col-md-6 ">
-            <div class="row gap-5 ps-2">
-                <h1 class="display-4">Don't concert about <strong>equity splitting</strong> anymore!</h1>
+            <div class="row gap-3 ps-2">
+                <h2 class="display-4">Don't concert about <strong>equity splitting</strong> anymore!</h2>
                 <p> start your business without any headache of equity splitting between cofounders. DEC (dynamic equity calculator) will do it for you instead. just focus on more important stuff and grow your business.</p>
                 <div class="row">
-                    <div class="col-6 col-md-4"><button class="btn btn-lg btn-dark">get started<i class="bi bi-arrow-right ps-2"></i> </button></div>
+                    <div class="col-6 col-md-5"><button class="btn btn-lg btn-dark">get started<i class="bi bi-arrow-right ps-2"></i> </button></div>
                     <div class="col-6 col-md-4"><button class="btn btn-lg btn-outline-dark" style="margin-left:-1.5rem;">more info<i class="bi bi-arrow-right ps-2"></i> </button></div>
                 </div>
             </div>
@@ -31,23 +31,44 @@
 <div class="container" style="padding:5.5rem;">
     <div class="row justify-content-center">
         <div class="col-6">
-            <h2 class="display-6">🔥 benefits of using DEC 🔥</h2>
+            <h2 class="display-6 pb-5">🔥 benefits of using DEC 🔥</h2>
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-6 col-md-4">
+<!-- benefits tiles -->
+    <div class="row justify-content-center">
+        <div class="col-6 pt-3">
             <div class="benefits">
                 <img src="assets\images\pie-chart.png" class="benefits-icons" alt="pie chart icon">
                 <div class="circle"></div>
-
+                <h3 class="benefits-title p-4">Cofounders fair equity splitting</h3>
+                <p class="benefits-desc pe-4 pb-4 ps-4">anyone takes equity by amout of money and time spent on business and some other important factors. It needs to be completely fair.</p>
+            </div>
+            <div class="benefits mt-5">
+                <img src="assets\images\bar-chart.png" class="benefits-icons" alt="pie chart icon">
+                <div class="circle"></div>
+                <h3 class="benefits-title p-4">Cofounders fair equity splitting</h3>
+                <p class="benefits-desc pe-4 pb-4 ps-4">anyone takes equity by amout of money and time spent on business and some other important factors. It needs to be completely fair.</p>
             </div>
         </div>
-        <div class="col-6 col-md-4"></div>
+        <div class="col-6 pt-3">
+            <div class="benefits">
+                <img src="assets\images\group.png" class="benefits-icons" alt="pie chart icon">
+                <div class="circle"></div>
+                <h3 class="benefits-title p-4">Easier team management</h3>
+                <p class="benefits-desc pe-4 pb-4 ps-4">Don't worry about the times someone leaves your team or a new person joins. you can continue calculating equities with DEC.</p>
+            </div>
+            <div class="benefits mt-5">
+                <img src="assets\images\file.png" class="benefits-icons" alt="pie chart icon">
+                <div class="circle"></div>
+                <h3 class="benefits-title p-4">Easier team management</h3>
+                <p class="benefits-desc pe-4 pb-4 ps-4">Don't worry about the times someone leaves your team or a new person joins. you can continue calculating equities with DEC.</p>
+            </div>
+        </div>
     </div>
 </div>
 
-<!-- benefits tiles -->
+
 
 
 
